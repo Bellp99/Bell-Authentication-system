f53276c608e9497fdee8f803f7cb94d0ad1fa636
Anabell <anabekk726@gmail.com>
2025-06-09 04:21:26 +0000

    .

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Invoice
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token 
@@ -19,15 +19,40 @@ CORS(api)
 
 #Create a route to authenticate your users and return JWTs
 #Create access token() function is used to actually generate the JWT
-@api.route('/login', methods=['POST'])
-def login():
-    username = request.json.get("username", None)
-    password = request.json.get("pasword", None)
+@api.route('/token', methods=['POST'])
+def generate_token():
 
-    if username != "sammysmith1234" or password != "abc123":
-        return jsonify({"msg": "Bad username or password"}), 401
-    access_token = create_access_token(identify=username)
-    return jsonify(access_token=access_token)
+    #login credentials
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    #Query the DB if the user email exists
+    email = email.lower()
+    user = User.query.filter_by(email=email).first()
+
+    #create a conditionn if the user does NOT exist
+    if user is None:
+        raise APIException('Sorry email or password not found', status_code=404)
+    elif user is not None and user.password != password:
+        raise APIException('Sorry email or password nor found', status_code=404)
+    #create a condition if the user does not exist
+    access_token = create_access_token(identity=user.id)
+    response = {
+        'access_token': access_token,
+        'user_id': user.id,
+        'message': f'Welcome {user.email}!'
+    }
+    return jsonify(response), 200
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
     
 #HW
 #Create a signup route and test it on postman
